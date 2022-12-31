4fb11d96064c0ea7b31ff458249bdfbaf123f395
TheFallenSpirit <thefallenspirit@outlook.com>
2022-12-30 23:48:53 -0800

    Partial code fix and cleanup

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,76 +1,79 @@
-import {Component, createEffect, createSignal, For, onMount} from "solid-js";
+import {Component, createSignal, onMount} from "solid-js";
 import PageBlock from "@/elements/PageBlock";
 import {useParams} from "@solidjs/router";
-import plyr from 'plyr';
+import Plyr from 'plyr';
 import Hls from "hls.js";
 import getInfo, {Anime} from "@/api/info";
 import getEpisodes, {Episode} from "@/api/info/episodes";
 import {createStore} from "solid-js/store";
-import getStreams, {Stream} from "@/api/streams";
+import getStreams from "@/api/streams";
+import {HiSolidFastForward, HiSolidRewind} from "solid-icons/hi";
 
 const Watch: Component = () => {
-	let currentEP = 0;
+	new Plyr('#player');
+	const hls = new Hls();
 	const { sId } = useParams();
 	const [info, setInfo] = createSignal<Anime|undefined>();
-	const [streams, setStreams] = createSignal<Stream[]|undefined>();
+	const [episode, setEpisode] = createSignal<number|undefined>();
 	const [data, setData] = createStore<{ total: number; episodes?: Episode[]; }>({ total: 0 });
-	const [current, setCurrent] = createSignal(1);
-	const [quality, setQuality] = createSignal('');
+
 	onMount(async () => {
 		await getInfo(parseInt(sId)).then((res) => setInfo(res.data));
 		await getEpisodes(parseInt(sId)).then((res) => setData({ total: res.data.total, episodes: res.data.episodes }));
-		if (!data.episodes) console.log('fuck you');
-		// @ts-ignore
-		await getStreams(data.episodes[currentEP].id).then((res) => setStreams(res.data));
+		if (!data.episodes) return;
 		if (!Hls.isSupported()) return;
-		const player = document.getElementById('player') as HTMLMediaElement;
-		if (!player) return;
-		const hls = new Hls();
-		const stream = streams()!.find((stream) => stream.quality === "1080p");
-		if (!stream) return;
-		await setQuality(stream.quality);
-		hls.loadSource(stream!.url);
-		createEffect(() => {
-			hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
-				new plyr(player);
-			});
-		});
-		hls.attachMedia(player);
+		await setEp(1, true);
 	});
 
-	const handleClick = async function (e: any) {
-		currentEP = e.target.getAttribute("ep-number") as number;
-		setCurrent(currentEP);
-		const meow = await getStreams(data.episodes![currentEP-1].id).then((res) => setStreams(res.data));
+	const setEp = async (ep: number, first: boolean): Promise<void> => {
+		setEpisode(ep);
 		const player = document.getElementById('player') as HTMLMediaElement;
-		if (!player) return;
-		const hls = new Hls();
-		const stream = quality() === '1080p' ? streams()!.find((stream) => stream.quality === "1080p") : streams()!.find((stream) => stream.quality === "720p");
-		hls.loadSource(stream!.url);
-		createEffect(() => {
-			hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
-				new plyr(player);
-			});
+		console.log('function init');
+		if (!data.episodes) return console.log('ep failed');
+		await getStreams(data.episodes[ep-1].id).then(async (res) => {
+			console.log('streams loaded');
+			if (!player) return console.log('player failed');
+			const stream = res.data.filter((s) => s.quality === '1080p' || s.quality === '720p')[0].url;
+			console.log(stream);
+			if (!stream) console.log('stream failed');
+			await hls.loadSource(stream);
+			console.log('source loaded');
+			if () {
+				hls.on(Hls.Events.MANIFEST_PARSED, async () => {
+					if (first) {
+						hls.attachMedia(player);
+						console.log('attached and first');
+					}
+				});
+			} else console.log('NOT FIRST');
 		});
-		hls.attachMedia(player);
 	};
 
 	return (
 		<PageBlock title={'watch'}>
-			<div class={'flex flex-col'}>
-				<h1 class={'mb-4'}>{info()?.title}</h1>
-				<video id={'player'} class={'w-3/4'} controls style={{'max-height': 'calc(100vh - 200px)'}}></video>
-			</div>
-
-			<div class={'flex justify-start items-start mt-4'}>
-				<div class={'flex flex-wrap justify-around mt-6 gap-x-2 gap-y-5 sm:justify-between'}>
-					<For each={data.episodes}>
-						{(episode) => (
-							<button class={`${current() == episode.number ? 'bg-[#6FB28F] text-[#232323]' : 'bg-[#3A6281]'} text-white rounded-md w-12 h-8 text-xl font-bold`} onClick={handleClick} ep-number={episode.number}>{episode.number}</button>
-						)}
-					</For>
+			<div class={'flex flex-col h-full'}>
+				<h1 class={'mb-6'}>{info()?.title} | Ep: {episode()}</h1>
+				<video id={'player'} class={'w-full max-w-6xl'} preload={'none'} controls/>
+				<div class={'flex flex-col w-full max-w-6xl bg-secondary h-32'}>
+					<div class={'flex justify-between h-8 bg-primary'}>
+						<span/>
+						<div class={'inline-flex px-3'}>
+							<HiSolidRewind size={28} onClick={() => setEp(episode()!-1, false)}/>&nbsp;
+							<HiSolidFastForward size={28} onClick={async () => await setEp(episode()!+1, false)}/>
+						</div>
+					</div>
 				</div>
 			</div>
+
+			{/*<div class={'flex justify-start items-start mt-4'}>*/}
+			{/*	<div class={'flex flex-wrap justify-around mt-6 gap-x-2 gap-y-5 sm:justify-between'}>*/}
+			{/*		<For each={data.episodes}>*/}
+			{/*			{(episode) => (*/}
+			{/*				<button class={`${current() == episode.number ? 'bg-[#6FB28F] text-[#232323]' : 'bg-[#3A6281]'} text-white rounded-md w-12 h-8 text-xl font-bold`} onClick={handleClick} ep-number={episode.number}>{episode.number}</button>*/}
+			{/*			)}*/}
+			{/*		</For>*/}
+			{/*	</div>*/}
+			{/*</div>*/}
 		</PageBlock>
 	);
 };
