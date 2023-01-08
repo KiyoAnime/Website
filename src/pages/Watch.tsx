66e9f83f5943179d49bd1608703466d949c5a408
TheFallenSpirit <thefallenspirit@outlook.com>
2023-01-08 09:43:24 -0800

    Enforce tailwind styling; Fix episode selector; optimize

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -10,6 +10,7 @@ import {Icon} from "solid-heroicons";
 import {chevronRight, chevronLeft, square_3Stack_3d, calendar} from "solid-heroicons/outline";
 import {backward, forward} from "solid-heroicons/solid";
 import classNames from "classnames";
+import Loader from "@/components/Loader";
 
 interface Embedded {
 	url: string;
@@ -19,6 +20,7 @@ interface Embedded {
 const Watch: Component = () => {
 	const { sId } = useParams();
 	const [loading, setLoading] = createSignal(true);
+	const [eLoading, setELoading] = createSignal(true);
 	const [info, setInfo] = createSignal<Anime | undefined>();
 	const [episode, setEpisode] = createSignal<number | undefined>();
 	const [embedded, setEmbedded] = createSignal<Embedded|undefined>();
@@ -31,21 +33,19 @@ const Watch: Component = () => {
 			setRange({ start: res.data.episodes![0].number, end: range.perPage });
 		});
 		setLoading(false);
-		await setEp(1, true);
+		await setEp(1);
 	});
 
-	const setEp = async (ep: number, first?: boolean): Promise<void> => {
-		const player = document.getElementById('player') as HTMLVideoElement;
-		if (!player) return;
-		const source = player.getElementsByTagName('source')[0];
+	const setEp = async (ep: number): Promise<void> => {
 		if (!info()?.episodes) return;
 		await getUrl(info()?.episodes![ep - 1].id!).then(async (res) => {
 			setEpisode(ep);
 			if (res.data.url) {
+				const player = document.getElementById('player') as HTMLVideoElement;
+				if (!player) return;
 				const hls = new Hls({ maxBufferLength: 30, maxBufferSize: 5242880, maxMaxBufferLength: 30 });
 				const plyr = new Plyr('#player', { controls: ['play-large', 'play', 'volume', 'progress', 'current-time', 'mute', 'settings', 'pip', 'airplay', 'fullscreen'] });
-				console.log('m3u8')
-				source.setAttribute('src', res.data.url);
+				player.src = res.data.url;
 				await hls.loadSource(res.data.url);
 				hls.on(Hls.Events.MANIFEST_PARSED, () => {
 					plyr.play();
@@ -59,25 +59,24 @@ const Watch: Component = () => {
 
 	const startEmbedded = (url: string) => {
 		setEmbedded({ url: url, enabled: true });
-		console.log(embedded()?.url)
 		const player = document.getElementById('embedded-player') as HTMLIFrameElement;
 		if (!player) return;
-		console.log('setting height');
-		player.height = player.contentWindow?.document.body.scrollHeight.toString() || '648';
+		player.src = url;
 	};
 
 	return (
 		<PageBlock title={'Kiyo'} loading={loading()}>
 			<div class={'flex flex-row justify-between mt-4'}>
-				<div class={'flex flex-col h-full w-full max-w-6xl'}>
+				<div class={'flex flex-col w-full max-w-6xl'}>
 					<Switch>
 						<Match when={embedded()?.enabled} keyed={false}>
-							<iframe id={'embedded-player'} src={embedded()?.url} style={'overflow: hidden;'} height={'648'} width={'1152'} />
+							<div class={'h-[40.5rem]'}>
+								{/* @ts-ignore: deprecated but solves problem. */}
+								<iframe id={'embedded-player'} scrolling={'no'} allowfullscreen class={'h-full w-full overflow-hidden outline-none'}/>
+							</div>
 						</Match>
 						<Match when={!embedded()?.enabled} keyed={false}>
-							<video id={'player'} preload={'none'} poster={'https://media.tenor.com/64BYBgDG41QAAAAC/loading.gif'}>
-								<source src={''} type={'application/x-mpegURL'}/>
-							</video>
+							<video id={'player'} preload={'none'} poster={'https://media.tenor.com/64BYBgDG41QAAAAC/loading.gif'}/>
 						</Match>
 					</Switch>
 					<Show when={info()?.episodes} keyed={false}>
@@ -85,8 +84,8 @@ const Watch: Component = () => {
 							<div class={'flex h-8 justify-between bg-primary'}>
 								<span/>
 								<div class={'inline-flex items-center mx-3'}>
-									<Icon path={backward} class={'h-7 w-7 cursor-pointer'} onClick={() => setEp(episode()! - 1, false)}/>&nbsp;
-									<Icon path={forward} class={'h-7 w-7 cursor-pointer'} onClick={() => setEp(episode()! + 1, false)}/>
+									<Icon path={backward} class={'h-7 w-7 cursor-pointer'} onClick={() => setEp(episode()! - 1)}/>&nbsp;
+									<Icon path={forward} class={'h-7 w-7 cursor-pointer'} onClick={() => setEp(episode()! + 1)}/>
 								</div>
 							</div>
 							<div class={'inline-flex justify-between items-center w-full py-4'}>
@@ -123,7 +122,7 @@ const Watch: Component = () => {
 						</div>
 					</Show>
 				</div>
-				<div class={'flex flex-col hidden items-end ml-4 xl:flex'}>
+				<div class={'flex flex-col hidden items-end ml-4 2xl:flex'}>
 					<img src={info()?.thumbnail} alt={info()?.title} class={'h-80 rounded-lg'}/>
 					<h3 class={'mt-2'}>{info()?.title}</h3>
 					<span class={'inline-flex items-center'}>
