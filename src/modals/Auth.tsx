9124d58844f03a2010602f33d4ad77049f1043a6
TheFallenSpirit <thefallenspirit@outlook.com>
2023-01-24 11:18:49 -0800

    Absolute buttons; Watch page work

--- src/modals/Auth.tsx
+++ src/modals/Auth.tsx
@@ -11,11 +11,12 @@ import Form, {Value} from "@/components/Form";
 import classNames from "classnames";
 import banner from '@/assets/banner.png';
 import {inputError} from "@/components/Form/Form";
+import Btn from "@/components/Button";
 
 const Auth: Component<{ open: boolean }> = (props)=> {
 	const [email, setEmail] = createSignal('');
 	const [view, setView] = createSignal('check');
-	const [style, setStyle] = createSignal('h-[21rem]');
+	const [style, setStyle] = createSignal('h-80');
 	const [title, setTitle] = createSignal('Welcome Back');
 	const [checkSubmitting, setCheckSubmitting] = createSignal(false);
 	const [loginSubmitting, setLoginSubmitting] = createSignal(false);
@@ -23,6 +24,7 @@ const Auth: Component<{ open: boolean }> = (props)=> {
 
 	const reset = () => {
 		clearFlash();
+		setStyle('h-80')
 		setView('check');
 	};
 
@@ -80,18 +82,25 @@ const Auth: Component<{ open: boolean }> = (props)=> {
 			<div class={'flex flex-col justify-center mt-6'}>
 				<Switch>
 					<Match when={view() === 'check'} keyed={false}>
-						<Form items={[{ id: 'email', type: 'email', label: 'Email', validation: { type: emailRegex, message: 'Please specify a valid email.' } }]} color={'bg-primary'} submitting={checkSubmitting()} onSubmit={checkSubmit} button={{ label: 'Next', color: 'blue' }}/>
+						<Form items={[{ id: 'email', type: 'email', label: 'Email', validation: { type: emailRegex, message: 'Please specify a valid email.' } }]} color={'bg-primary'} submitting={checkSubmitting()} onSubmit={checkSubmit}>
+							<div class={'absolute bottom-6 right-6'}>
+								<Btn.Blue type={'submit'} loading={checkSubmitting()}>Next</Btn.Blue>
+							</div>
+						</Form>
 					</Match>
 					<Match when={view() === 'login'} keyed={false}>
 						<Form items={[
 							{ id: 'email', type: 'email', label: 'Email', value: email(), readonly: true },
 							{ id: 'password', type: 'password', label: 'Password', validation: { type: 'string', message: 'You must specify a password.' } }
-						]} color={'bg-primary'} submitting={loginSubmitting()} onSubmit={loginSubmit} button={{ label: 'Login', color: 'blue' }}>
+						]} color={'bg-primary'} submitting={loginSubmitting()} onSubmit={loginSubmit}>
 							<span class={'flex justify-end'}>
 								<span class={'cursor-pointer hover:text-accent-blue'}>Forgot Password?</span>
 							</span>
 							<div class={'mt-3'}>
 								<HCaptcha id={'captcha'} sitekey={'cc87a83a-6974-4b66-93ba-08c9b0e1c1ac'} theme={'dark'}/>
+							</div>
+							<div class={'absolute bottom-6 right-6'}>
+								<Btn.Blue type={'submit'} loading={loginSubmitting()}>Login</Btn.Blue>
 							</div>
 						</Form>
 					</Match>
@@ -101,9 +110,12 @@ const Auth: Component<{ open: boolean }> = (props)=> {
 							{ id: 'username', type: 'text', label: 'Username', validation: { type: 'string', message: 'You must provide a username.' } },
 							{ id: 'password', type: 'password', label: 'Password', validation: { type: 'string', message: 'You must provide a password' } },
 							{ id: 'passwordConf', type: 'password', label: 'Confirm Password', validation: { type: 'string', message: 'You must confirm your password.' } }
-						]} color={'bg-primary'} submitting={registerSubmitting()} onSubmit={registerSubmit} button={{ color: 'blue', label: 'Create' }}>
+						]} color={'bg-primary'} submitting={registerSubmitting()} onSubmit={registerSubmit}>
 							<div class={'mt-5'}>
 								<HCaptcha sitekey={'cc87a83a-6974-4b66-93ba-08c9b0e1c1ac'} theme={'dark'}/>
+							</div>
+							<div class={'absolute bottom-6 right-6'}>
+								<Btn.Blue type={'submit'} loading={registerSubmitting()}>Register</Btn.Blue>
 							</div>
 						</Form>
 					</Match>
--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -63,10 +63,6 @@ const Watch: Component = () => {
 		})
 	};
 
-	const changeUrl = (id: string) => {
-		window.location.href = `/watch/${id}`;
-	};
-
 	const updateQuality = (quality: number) => {
 		if (!window.hls) return;
 		window.hls.levels.forEach((level, index) => {
@@ -78,89 +74,60 @@ const Watch: Component = () => {
 
 	return (
 		<PageBlock title={'Kiyo'} loading={loading()} flash={{ type: 'flex', key: 'watch' }}>
-			<div class={'flex flex-row justify-between mt-4'}>
-				<div class={'flex flex-col w-full max-w-6xl'}>
-					<Switch>
-						<Match when={playerMode() === 'kiyo'} keyed={false}>
-							<video id={'player'} autoplay controls poster={'https://media.tenor.com/64BYBgDG41QAAAAC/loading.gif'}/>
-						</Match>
-						<Match when={playerMode() === 'embedded'} keyed={false}>
-							<div class={'h-[40.5rem]'}>
-								{/* @ts-ignore: deprecated but solves problem. */}
-								<iframe id={'embedded-player'} scrolling={'no'} allowfullscreen class={'h-full w-full overflow-hidden outline-none'}/>
-							</div>
-						</Match>
-					</Switch>
-					<Show when={info()?.episodes} keyed={false}>
-						<div class={'flex flex-col w-full max-w-6xl bg-secondary'}>
-							<div class={'flex h-8 justify-between bg-primary'}>
-								<span/>
-								<div class={'inline-flex items-center mx-3'}>
-									<Icon path={backward} class={'h-7 w-7 cursor-pointer'} onClick={() => setEp(episode()! - 1)}/>&nbsp;
-									<Icon path={forward} class={'h-7 w-7 cursor-pointer'} onClick={() => setEp(episode()! + 1)}/>
-								</div>
-							</div>
-							<div class={'inline-flex justify-between items-center w-full py-4'}>
-								<div class={'md:w-1/12'}>
-									<Switch>
-										<Match when={range.end < range.perPage * 2} keyed={false}>
-											<Icon path={chevronLeft} class={'h-14 w-14 text-gray-500 cursor-not-allowed'}/>
-										</Match>
-										<Match when={range.end >= range.perPage * 2} keyed={false}>
-											<Icon path={chevronLeft} class={'h-14 w-14 cursor-pointer'} onClick={() => setRange({ start: range.start - range.perPage, end: range.end - range.perPage })}/>
-										</Match>
-									</Switch>
-								</div>
-								<div class={'flex flex-wrap justify-between gap-[4px] md:justify-start'}>
-									<For each={info()?.episodes}>
-										{(e) => (
-											<Show when={e.number >= range.start && e.number <= range.end} keyed={false}>
-												<button class={classNames('w-12 h-8 bg-cyan-700 text-gray-100 rounded', e.number === episode() && 'bg-accent-pink')} onClick={() => setEp(e.number)}>{e.number}</button>
-											</Show>
-										)}
-									</For>
-								</div>
-								<div class={'md:w-1/12'}>
-									<Switch>
-										<Match when={range.end >= info()!.episodes!.length+1} keyed={false}>
-											<Icon path={chevronRight} class={'h-14 w-14 text-gray-500 cursor-not-allowed'}/>
-										</Match>
-										<Match when={range.end < info()!.episodes!.length+1} keyed={false}>
-											<Icon path={chevronRight} class={'h-14 w-14 cursor-pointer'} onClick={() => setRange({ ...range, start: range.start + range.perPage, end: range.end + range.perPage })}/>
-										</Match>
-									</Switch>
-								</div>
+			<div class={'flex flex-col w-full mt-4 max-w-6xl'}>
+				<Switch>
+					<Match when={playerMode() === 'kiyo'} keyed={false}>
+						<video id={'player'} autoplay controls poster={'https://media.tenor.com/64BYBgDG41QAAAAC/loading.gif'}/>
+					</Match>
+					<Match when={playerMode() === 'embedded'} keyed={false}>
+						<div class={'h-[40.5rem]'}>
+							{/* @ts-ignore: deprecated but solves problem. */}
+							<iframe id={'embedded-player'} scrolling={'no'} allowfullscreen class={'h-full w-full overflow-hidden outline-none'}/>
+						</div>
+					</Match>
+				</Switch>
+				<Show when={info()?.episodes} keyed={false}>
+					<div class={'flex flex-col w-full bg-secondary'}>
+						<div class={'flex h-8 justify-between bg-primary'}>
+							<span/>
+							<div class={'inline-flex items-center mx-3'}>
+								<Icon path={backward} class={'h-7 w-7 cursor-pointer'} onClick={() => setEp(episode()! - 1)}/>&nbsp;
+								<Icon path={forward} class={'h-7 w-7 cursor-pointer'} onClick={() => setEp(episode()! + 1)}/>
 							</div>
 						</div>
-					</Show>
-				</div>
-				<div class={'hidden flex-col w-1/3 items-end ml-4 2xl:flex'}>
-					<img src={info()?.thumbnail} alt={info()?.title} class={'h-80 rounded-lg'}/>
-					<h3 class={'mt-2 text-right line-clamp-1'}>{info()?.title}</h3>
-					<span class={'inline-flex items-center'}>
-						<Icon path={calendar} class={'h-4 w-4'}/>&nbsp;{info()?.released}&nbsp;
-						<Icon path={square_3Stack_3d} class={'h-4 w-4'}/>&nbsp;Episode: {episode()}
-					</span>
-					<div class="flex justify-center">
-						<label class={'my-2 ml-4'}>Watch Order &nbsp;&nbsp;</label>
-						<div class="flex flex-col w-full 2xl:w-2/3 gap-2 bg-primary text-gray-200">
-							<select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-200 bg-secondary bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-primary focus:border-blue-600 focus:outline-none" onChange={(e) => changeUrl(e.currentTarget.value)}>
-								<For each={info()?.watchOrder}>
-									{(e, i) => (
-										<Switch>
-											<Match when={e.id === parseInt(id)} keyed={false}>
-												<option value={e.id} selected>{e.name}</option>
-											</Match>
-											<Match when={e.id !== parseInt(id)} keyed={false}>
-												<option value={e.id}>{e.name}</option>
-											</Match>
-										</Switch>
+						<div class={'inline-flex justify-between items-center w-full py-4'}>
+							<div class={'md:w-1/12'}>
+								<Switch>
+									<Match when={range.end < range.perPage * 2} keyed={false}>
+										<Icon path={chevronLeft} class={'h-14 w-14 text-gray-500 cursor-not-allowed'}/>
+									</Match>
+									<Match when={range.end >= range.perPage * 2} keyed={false}>
+										<Icon path={chevronLeft} class={'h-14 w-14 cursor-pointer'} onClick={() => setRange({ start: range.start - range.perPage, end: range.end - range.perPage })}/>
+									</Match>
+								</Switch>
+							</div>
+							<div class={'flex flex-wrap justify-between gap-[4px] md:justify-start'}>
+								<For each={info()?.episodes}>
+									{(e) => (
+										<Show when={e.number >= range.start && e.number <= range.end} keyed={false}>
+											<button class={classNames('w-12 h-8 bg-cyan-700 text-gray-100 rounded', e.number === episode() && 'bg-accent-pink')} onClick={() => setEp(e.number)}>{e.number}</button>
+										</Show>
 									)}
 								</For>
-							</select>
+							</div>
+							<div class={'md:w-1/12'}>
+								<Switch>
+									<Match when={range.end >= info()!.episodes!.length+1} keyed={false}>
+										<Icon path={chevronRight} class={'h-14 w-14 text-gray-500 cursor-not-allowed'}/>
+									</Match>
+									<Match when={range.end < info()!.episodes!.length+1} keyed={false}>
+										<Icon path={chevronRight} class={'h-14 w-14 cursor-pointer'} onClick={() => setRange({ ...range, start: range.start + range.perPage, end: range.end + range.perPage })}/>
+									</Match>
+								</Switch>
+							</div>
 						</div>
 					</div>
-				</div>
+				</Show>
 			</div>
 		</PageBlock>
 	);
